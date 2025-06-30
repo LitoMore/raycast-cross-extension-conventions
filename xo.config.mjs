9aee852e41cd5ce268bc45f2a89899eaf23d0c2a
LitoMore <litomore@gmail.com>
2025-07-01 00:32:53 +0800

    Bump dependencies

---
+++ xo.config.mjs
@@ -0,0 +1,33 @@
+const xoConfig = [
+	{
+		prettier: true,
+		rules: {
+			'sort-imports': [
+				'error',
+				{
+					ignoreCase: false,
+					ignoreDeclarationSort: true,
+					ignoreMemberSort: false,
+					memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
+					allowSeparatedGroups: false,
+				},
+			],
+			'import-x/no-named-as-default': 'off',
+			'import-x/extensions': 'off',
+			'import-x/order': [
+				'error',
+				{
+					groups: ['builtin', 'external', 'parent', 'sibling', 'index'],
+					alphabetize: {
+						order: 'asc',
+						caseInsensitive: true,
+					},
+					warnOnUnassignedImports: true,
+					'newlines-between': 'never',
+				},
+			],
+		},
+	},
+];
+
+export default xoConfig;
